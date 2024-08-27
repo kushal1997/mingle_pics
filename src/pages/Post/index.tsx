53d12055c6d43aa7d9643732806225d11ef0454f
kushal1997 <kushalrao103@gmail.com>
2024-08-27 19:42:39 +0530

    sidebar synced with available routes

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,14 @@
+import Layout from "@/components/layout";
 import * as React from "react";
 
 interface IPostProps {}
 
-const Post: React.FunctionComponent<IPostProps> = (props) => {
-  return <div>Post</div>;
+const Post: React.FunctionComponent<IPostProps> = () => {
+  return (
+    <Layout>
+      <div>Post</div>
+    </Layout>
+  );
 };
 
 export default Post;
