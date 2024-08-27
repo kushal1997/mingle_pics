59305727a96807d432d9ead08144a875c0232e95
kushal1997 <kushalrao103@gmail.com>
2024-08-27 18:20:55 +0530

    intial layout setup

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface ISidebarProps {}
+
+const Sidebar: React.FunctionComponent<ISidebarProps> = (props) => {
+  return <>Sidebar</>;
+};
+
+export default Sidebar;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
+import Layout from "@/components/layout";
 import * as React from "react";
 
 interface IHomeProps {}
 
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
   return (
-    <>
+    <Layout>
       <div>home</div>
-    </>
+    </Layout>
   );
 };
 
