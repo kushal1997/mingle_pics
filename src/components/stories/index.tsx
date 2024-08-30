e9f1cc574a6346469a1fa57b8e867f5954423bc2
kushal1997 <kushalrao103@gmail.com>
2024-08-30 10:13:43 +0530

    tried stories with avatar but not working properly

--- src/components/stories/index.tsx
+++ src/components/stories/index.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { img1, img2, img3 } from "@/assets/images";
 
 interface IStoriesProps {}
 
 const Stories: React.FunctionComponent<IStoriesProps> = () => {
   return (
     <>
-      <h1>Stories</h1>
+      <div className="flex justify-between">
+        <Avatar className="h-20 w-20 border-4 border-slate-800 object-cover">
+          <AvatarImage src={img1} alt="@shadcn" />
+        </Avatar>
+
+        <Avatar className="h-20 w-20 border-4 border-slate-800 object-cover">
+          <AvatarImage src={img2} alt="@shadcn" />
+        </Avatar>
+
+
+        <Avatar className="h-20 w-20 border-4 border-slate-800 object-cover">
+          <AvatarImage src={img3} alt="@shadcn" />
+        </Avatar>
+      </div>
     </>
   );
 };
