323a57bb6bad8aca8cf4033794f324df13ba2ea2
kushal1997 <kushalrao103@gmail.com>
2024-08-30 10:17:07 +0530

    stories ui done with demo data

--- src/components/stories/index.tsx
+++ src/components/stories/index.tsx
@@ -1,25 +1,16 @@
 import * as React from "react";
-import { Avatar, AvatarImage } from "../ui/avatar";
-import { img1, img2, img3 } from "@/assets/images";
+import { img1, img2, img3, img4 } from "@/assets/images";
 
 interface IStoriesProps {}
 
 const Stories: React.FunctionComponent<IStoriesProps> = () => {
   return (
     <>
-      <div className="flex justify-between">
-        <Avatar className="h-20 w-20 border-4 border-slate-800 object-cover">
-          <AvatarImage src={img1} alt="@shadcn" />
-        </Avatar>
-
-        <Avatar className="h-20 w-20 border-4 border-slate-800 object-cover">
-          <AvatarImage src={img2} alt="@shadcn" />
-        </Avatar>
-
-
-        <Avatar className="h-20 w-20 border-4 border-slate-800 object-cover">
-          <AvatarImage src={img3} alt="@shadcn" />
-        </Avatar>
+      <div className="flex gap-5">
+      <img src={img1} alt="" className="h-20 w-20 rounded-full border-4 border-slate-800 object-cover" />
+      <img src={img2} alt="" className="h-20 w-20 rounded-full border-4 border-slate-800 object-cover" />
+      <img src={img3} alt="" className="h-20 w-20 rounded-full border-4 border-slate-800 object-cover" />
+      <img src={img4} alt="" className="h-20 w-20 rounded-full border-4 border-slate-800 object-cover" />
       </div>
     </>
   );
