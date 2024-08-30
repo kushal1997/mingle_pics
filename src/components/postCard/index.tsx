6bb920a2ea1bcb9ca16485a5033db00a68e86663
kushal1997 <kushalrao103@gmail.com>
2024-08-30 12:06:07 +0530

    added like/dislike feature

--- src/components/postCard/index.tsx
+++ src/components/postCard/index.tsx
@@ -1,15 +1,49 @@
 import { DocumentResponse } from "@/types";
 import * as React from "react";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 import { img1 } from "@/assets/images";
 import { HeartIcon, MessageCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useUserAuth } from "@/context/userAuthContext";
+import { updateLikesOnPost } from "@/repository/post.service";
 
 interface IPostCardProps {
   data: DocumentResponse;
 }
+type LikesInfoType = {
+  likes: number;
+  isLike: boolean;
+};
 
 const PostCard: React.FunctionComponent<IPostCardProps> = ({ data }) => {
+  const { user } = useUserAuth();
+  const [likesInfo, setLikesInfo] = React.useState<LikesInfoType>({
+    likes: data.likes!,
+    isLike: data.userlikes?.includes(user?.uid) ? true : false,
+  });
+
+  const updateLike = async (isVal: boolean) => {
+    setLikesInfo({
+      likes: isVal ? likesInfo.likes + 1 : likesInfo.likes - 1,
+      isLike: !likesInfo.isLike,
+    });
+    if (isVal) {
+      data.userlikes?.push(user?.uid);
+    } else {
+      data.userlikes?.splice(data.userlikes.indexOf(user?.uid));
+    }
+    await updateLikesOnPost(
+      data.id!,
+      data.userlikes!,
+      isVal ? likesInfo.likes + 1 : likesInfo.likes - 1
+    );
+  };
   return (
     <>
       <Card className="mb-6">
@@ -26,17 +60,24 @@ const PostCard: React.FunctionComponent<IPostCardProps> = ({ data }) => {
           </CardTitle>
         </CardHeader>
         <CardContent className="p-0">
-<img src={data.photos ? data.photos[0].cdnUrl :''} alt="" />
+          <img src={data.photos ? data.photos[0].cdnUrl : ""} alt="" />
         </CardContent>
         <CardFooter className="flex flex-col p-3">
-<div className="flex justify-between w-full mb-3">
-    <HeartIcon className={cn("mr-3","cursor-pointer")}/>
-    <MessageCircle className="mr-3"/>
-</div>
-<div className="w-full text-sm">{0} Likes</div>
-<div className="w-full text-sm">
-    <span>Guest User</span>: {data.caption}
-</div>
+          <div className="flex justify-between w-full mb-3">
+            <HeartIcon
+              className={cn(
+                "mr-3",
+                "cursor-pointer",
+                likesInfo.isLike ? "fill-red-500" : "fill-none"
+              )}
+              onClick={() => updateLike(!likesInfo.isLike)}
+            />
+            <MessageCircle className="mr-3" />
+          </div>
+          <div className="w-full text-sm">{likesInfo.likes} Likes</div>
+          <div className="w-full text-sm">
+            <span>Guest User</span>: {data.caption}
+          </div>
         </CardFooter>
       </Card>
     </>
--- src/repository/post.service.ts
+++ src/repository/post.service.ts
@@ -1,47 +1,69 @@
 import { db } from "@/firebaseConfig";
 import { DocumentResponse, PostType } from "@/types";
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, orderBy, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 
-const COLLECTION_NAME="posts";
+const COLLECTION_NAME = "posts";
 
-export const createPost=(post:PostType)=>{
-    return addDoc(collection(db,COLLECTION_NAME),post);
-}
+export const createPost = (post: PostType) => {
+  return addDoc(collection(db, COLLECTION_NAME), post);
+};
 
-export const getPosts=async()=>{
-try {
-    const q =query(collection(db,COLLECTION_NAME),orderBy("date","desc"));
-    const querySnapshot=await getDocs(q);
+export const getPosts = async () => {
+  try {
+    const q = query(collection(db, COLLECTION_NAME), orderBy("date", "desc"));
+    const querySnapshot = await getDocs(q);
     const tempArr: DocumentResponse[] = [];
-    if(querySnapshot.size >0){
-        querySnapshot.forEach(doc=>{
-            const data=doc.data() as PostType;
-            const responseObj:DocumentResponse={
-                id:doc.id,
-                ...data,
-            }
-            tempArr.push(responseObj);
-        })
-        return tempArr
-    }else{
-        console.error("No such document");
-        
+    if (querySnapshot.size > 0) {
+      querySnapshot.forEach((doc) => {
+        const data = doc.data() as PostType;
+        const responseObj: DocumentResponse = {
+          id: doc.id,
+          ...data,
+        };
+        tempArr.push(responseObj);
+      });
+      return tempArr;
+    } else {
+      console.error("No such document");
     }
-} catch (error) {
+  } catch (error) {
     console.error(error);
-}   
-}
+  }
+};
 
-export const getPostByUserId=(id:string)=>{
-    const q =query(collection(db,COLLECTION_NAME),where("userId","==",id));
-    return getDocs(q)
-}
+export const getPostByUserId = (id: string) => {
+  const q = query(collection(db, COLLECTION_NAME), where("userId", "==", id));
+  return getDocs(q);
+};
 
-export const getPost=(id:string)=>{
-    const docRef=doc(db,COLLECTION_NAME,id);
-    return getDoc(docRef)
-}
+export const getPost = (id: string) => {
+  const docRef = doc(db, COLLECTION_NAME, id);
+  return getDoc(docRef);
+};
 
-export const deletePost=(id:string)=>{
-    return deleteDoc(doc(db,COLLECTION_NAME,id))
-}
+export const deletePost = (id: string) => {
+  return deleteDoc(doc(db, COLLECTION_NAME, id));
+};
+
+export const updateLikesOnPost = (
+  id: string,
+  userLikes: string[],
+  likes: number
+) => {
+  const docRef = doc(db, COLLECTION_NAME, id);
+  return updateDoc(docRef, {
+    likes: likes,
+    userlikes: userLikes,
+  });
+};
